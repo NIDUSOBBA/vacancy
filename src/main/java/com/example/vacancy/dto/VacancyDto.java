c280d16d08b9add48d25c55d406561915f2c8457
aubba <abublikbrazers@gmail.com>
2026-02-02 19:53:16 +0300

    refactor (dto) Поменяли поля добавили валидацию и билдер

--- src/main/java/com/example/vacancy/dto/VacancyDto.java
+++ src/main/java/com/example/vacancy/dto/VacancyDto.java
@@ -1,6 +1,21 @@
 package com.example.vacancy.dto;
 
-public record VacancyDto(String nameVacancy,
-                         String fieldActivity,
-                         String description) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+
+@Builder
+public record VacancyDto(
+
+        @NotBlank(message = "Название вакансии обязательно")
+        String name,
+        @NotBlank(message = "Описание вакансии обязательно")
+        String description,
+        @NotBlank(message = "Название компании обязательно")
+        String company,
+        @NotBlank(message = "Расположение компании обязательно")
+        String location,
+        @Positive(message = "Зарплата должна быть положительной")
+        Integer salary,
+        String workExperience) {
 }
