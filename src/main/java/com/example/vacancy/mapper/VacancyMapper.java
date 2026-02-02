c87f572aa27b6060258ccae9ecdc25747f394cc5
aubba <abublikbrazers@gmail.com>
2026-02-02 19:54:35 +0300

    refactor (mapper) Использования билдера вместо new

--- src/main/java/com/example/vacancy/mapper/VacancyMapper.java
+++ src/main/java/com/example/vacancy/mapper/VacancyMapper.java
@@ -1,44 +1,55 @@
 package com.example.vacancy.mapper;
 
 import com.example.vacancy.dto.DeleteVacancyDto;
+import com.example.vacancy.dto.UpdateVacancyDto;
 import com.example.vacancy.dto.VacancyDto;
 import com.example.vacancy.entity.Vacancy;
+import com.example.vacancy.util.Status;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class VacancyMapper {
 
     public VacancyDto vacancyToDto(Vacancy vacancy) {
-        return new VacancyDto(
-                vacancy.getNameVacancy(),
-                vacancy.getFieldActivity(),
-                vacancy.getDescription());
+        return VacancyDto.builder()
+                .name(vacancy.getNameVacancy())
+                .description(vacancy.getDescription())
+                .company(vacancy.getCompany())
+                .location(vacancy.getLocation())
+                .salary(vacancy.getSalary())
+                .workExperience(vacancy.getWorkExperience())
+                .build();
     }
-    public DeleteVacancyDto vacancyToDeleteDto(Vacancy vacancy) {
-        return new DeleteVacancyDto(
-                "В путь",
-                vacancy.getId(),
-                vacancy.getNameVacancy(),
-                vacancy.getFieldActivity(),
-                vacancy.getDescription());
+
+    public DeleteVacancyDto vacancyToDeleteDto(Long id) {
+        return DeleteVacancyDto.builder()
+                .status(Status.LIQUIDATED)
+                .id(id)
+                .build();
     }
 
     public Vacancy vacancyDtoToEntity(VacancyDto vacancyDto) {
-        return new Vacancy(
-                vacancyDto.nameVacancy(),
-                vacancyDto.fieldActivity(),
-                vacancyDto.description());
+        return Vacancy.builder()
+                .nameVacancy(vacancyDto.name())
+                .description(vacancyDto.description())
+                .company(vacancyDto.company())
+                .location(vacancyDto.location())
+                .salary(vacancyDto.salary())
+                .workExperience(vacancyDto.workExperience())
+                .build();
     }
 
-    public List<VacancyDto> vacancyToDto(List<Vacancy> vacancies) {
-        List<VacancyDto> vacancyDto = new ArrayList<>();
-        for (Vacancy vacancy : vacancies) {
-            vacancyDto.add(vacancyToDto(vacancy));
-        }
-        return vacancyDto;
+    public List<VacancyDto> vacancyToDto(Page<Vacancy> vacancies) {
+        return vacancies.map(this::vacancyToDto).stream().toList();
     }
 
+    public Vacancy updateDtoToVacancy(Vacancy byId, UpdateVacancyDto updateVacancyDto) {
+        byId.setNameVacancy(updateVacancyDto.name());
+        byId.setDescription(updateVacancyDto.description());
+        byId.setSalary(updateVacancyDto.salary());
+        return byId;
+    }
 }
