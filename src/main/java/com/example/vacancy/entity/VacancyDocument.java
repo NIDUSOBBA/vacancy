29df02cd302c96ae52277bf4707ea897bc2aebcf
aubba <abublikbrazers@gmail.com>
2026-02-02 19:51:33 +0300

    refactor (entity) Поменял название полей и добавил билдер

--- src/main/java/com/example/vacancy/entity/VacancyDocument.java
+++ src/main/java/com/example/vacancy/entity/VacancyDocument.java
@@ -3,10 +3,7 @@ package com.example.vacancy.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -14,23 +11,33 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @Document(indexName = "vacancy")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class VacancyDocument {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Field(type = FieldType.Text)
     private String nameVacancy;
 
     @Field(type = FieldType.Text)
-    private String fieldActivity;
-
-    @Field(type = FieldType.Text)
     private String description;
 
+    @Field(type = FieldType.Text)
+    private String company;
+
+    @Field(type = FieldType.Text)
+    private String location;
+
     @Field(type = FieldType.Integer)
-    private Integer weight = 100;
+    private Integer salary;
+
+    @Field(type = FieldType.Text)
+    private String workExperience;
+
+    @Field(type = FieldType.Integer)
+    private Integer weight = 0;
 }
