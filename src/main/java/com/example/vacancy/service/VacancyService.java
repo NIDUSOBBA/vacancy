66eabe76458969db1f8c6a794034a9eb64a95b52
aubba <abublikbrazers@gmail.com>
2026-02-02 19:57:44 +0300

    refactor (service) Поменял названия для методов и добавил класс для вычисления веса вакансий

--- src/main/java/com/example/vacancy/service/VacancyService.java
+++ src/main/java/com/example/vacancy/service/VacancyService.java
@@ -1,21 +1,22 @@
 package com.example.vacancy.service;
 
-import com.example.vacancy.dto.DeleteVacancyDto;
-import com.example.vacancy.dto.UpdateVacancyDto;
-import com.example.vacancy.dto.VacancyDto;
+import com.example.vacancy.calculator.WeightCalculationResult;
+import com.example.vacancy.dto.*;
 import com.example.vacancy.entity.Vacancy;
 import com.example.vacancy.event.VacancyCreatedEvent;
 import com.example.vacancy.event.VacancyDeletedEvent;
-import com.example.vacancy.event.VacancyEventListener;
 import com.example.vacancy.event.VacancyUpdatedEvent;
+import com.example.vacancy.exception.VacancyNotFound;
 import com.example.vacancy.mapper.VacancyMapper;
 import com.example.vacancy.repository.VacancyRepository;
+import com.example.vacancy.calculator.VacancyWeightCalculator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Locale;
 
 @Service
 @RequiredArgsConstructor
@@ -24,56 +25,37 @@ public class VacancyService {
 
     private final VacancyMapper vacancyMapper;
     private final VacancyRepository vacancyRepository;
-    private final VacancyEventListener  vacancyEventListener;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final VacancyWeightCalculator vacancyWeightCalculator;
 
-    public VacancyDto createVacancy(VacancyDto vacancyDto) {
-        Vacancy save = vacancyRepository.save(fieldSearch(vacancyDto));
-        vacancyEventListener.handleVacancyCreated(new VacancyCreatedEvent(save));
+    public VacancyDto create(VacancyDto vacancyDto) {
+        Vacancy vacancy = vacancyMapper.vacancyDtoToEntity(vacancyDto);
+        WeightCalculationResult weightCalculationResult = vacancyWeightCalculator.calculateWeight(vacancy);
+        vacancy.setWeight(weightCalculationResult.getTotalWeight());
+        Vacancy save = vacancyRepository.save(vacancy);
+        applicationEventPublisher.publishEvent(new VacancyCreatedEvent(save));
         return vacancyMapper.vacancyToDto(save);
     }
 
-    public List<VacancyDto> getAllVacancies() {
-        return vacancyMapper.vacancyToDto(
-                vacancyRepository.findAll()
-        );
+    public List<VacancyDto> getAll(VacancyPageDto pageDto) {
+        return vacancyMapper.vacancyToDto(vacancyRepository.findAll(PageRequest.of(pageDto.page(), pageDto.size())));
     }
 
-    public VacancyDto getVacanciesById(Integer id) {
-        return vacancyMapper.vacancyToDto(
-                vacancyRepository.findById(id
-                )
-        );
+    public VacancyDto getById(Long id) {
+        return vacancyMapper.vacancyToDto(vacancyRepository.findById(id).orElseThrow(() -> new VacancyNotFound(id)));
     }
 
-    public VacancyDto updateVacancyById(UpdateVacancyDto updateVacancyDto) {
-        Vacancy byId = vacancyRepository.findById(updateVacancyDto.id());
-        byId.setNameVacancy(updateVacancyDto.nameVacancy());
-        byId.setFieldActivity(updateVacancyDto.fieldActivity());
-        byId.setDescription(updateVacancyDto.description());
-        vacancyEventListener.handleVacancyUpdated(new VacancyUpdatedEvent(byId));
-        return vacancyMapper.vacancyToDto(
-                vacancyRepository.update(byId)
-        );
+    public VacancyDto updateById(UpdateVacancyDto updateVacancyDto) {
+        Vacancy byId = vacancyRepository.findById(updateVacancyDto.id()).orElseThrow(() -> new VacancyNotFound(updateVacancyDto.id()));
+        Vacancy updatedVacancy = vacancyMapper.updateDtoToVacancy(byId, updateVacancyDto);
+        applicationEventPublisher.publishEvent(new VacancyUpdatedEvent(updatedVacancy));
+        return vacancyMapper.vacancyToDto(vacancyRepository.save(updatedVacancy));
     }
 
-    public DeleteVacancyDto deleteVacancyById(Integer id) {
-        Vacancy byId = vacancyRepository.findById(id);
-        vacancyRepository.delete(byId);
-        vacancyEventListener.handleVacancyDeleted(new VacancyDeletedEvent(byId.getId()));
-        return vacancyMapper.vacancyToDeleteDto(byId);
-    }
-
-    public Vacancy fieldSearch(VacancyDto vacancyDto) {
-        Vacancy vacancy = vacancyMapper.vacancyDtoToEntity(vacancyDto);
-        int field = vacancy.getWeight();
-        String fieldActivity = vacancy.getFieldActivity().toLowerCase();
-        switch (fieldActivity) {
-            case "муницип" -> field += 10;
-            case "гос" -> field += 20;
-            case "мед" -> field += 30;
-        }
-        vacancy.setWeight(field);
-        return vacancy;
+    public DeleteVacancyDto deleteById(Long id) {
+        vacancyRepository.deleteById(id);
+        applicationEventPublisher.publishEvent(new VacancyDeletedEvent(id));
+        return vacancyMapper.vacancyToDeleteDto(id);
     }
 
 }
