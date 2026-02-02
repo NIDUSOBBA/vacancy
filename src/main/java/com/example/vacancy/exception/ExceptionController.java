47470426c9dfbae942e5a309b1ab983064aaa4ca
aubba <abublikbrazers@gmail.com>
2026-02-02 19:41:27 +0300

    refactor (exception) Сделал класс который возвращается ExceptionHandler вместо маппера

--- src/main/java/com/example/vacancy/exception/ExceptionController.java
+++ src/main/java/com/example/vacancy/exception/ExceptionController.java
@@ -1,23 +1,27 @@
 package com.example.vacancy.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class ExceptionController {
 
-    private Map<String, String> exceptionWriter(Exception exception) {
-        Map<String, String> errors = new HashMap<>();
-        errors.put("error", exception.getClass().getSimpleName());
-        errors.put("message", exception.getMessage());
-        return errors;
+    private ExceptionMessage exceptionWriter(Exception exception) {
+        return ExceptionMessage.builder()
+                .errorClassName(exception.getClass().getSimpleName())
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ExceptionMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return exceptionWriter(exception);
     }
 
     @ExceptionHandler(VacancyNotFound.class)
-    public Map<String, String> handleVacancyNotFound(VacancyNotFound vacancyNotFound) {
+    public ExceptionMessage handleVacancyNotFound(VacancyNotFound vacancyNotFound) {
         return exceptionWriter(vacancyNotFound);
     }
+
 }
