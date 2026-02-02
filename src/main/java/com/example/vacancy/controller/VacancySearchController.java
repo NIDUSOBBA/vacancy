0d603187d000f91cceb99b5b5b7a2e33187c941e
aubba <abublikbrazers@gmail.com>
2026-02-02 19:56:59 +0300

    refactor (controller) Сделал один метод с параметром для сортировки и поменял путь так же добавил валидацию

--- src/main/java/com/example/vacancy/controller/VacancySearchController.java
+++ src/main/java/com/example/vacancy/controller/VacancySearchController.java
@@ -1,28 +1,25 @@
 package com.example.vacancy.controller;
 
+import com.example.vacancy.dto.ElasticsearchVacancyPageDto;
 import com.example.vacancy.dto.VacancyDocumentDto;
-import com.example.vacancy.service.VacancySearchService;
+import com.example.vacancy.service.ElasticsearchVacancySearchService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/vacancies/search")
+@RequestMapping("api/v1/vacancies/search")
 @RequiredArgsConstructor
+@Validated
 public class VacancySearchController {
-    private final VacancySearchService vacancySearchService;
+    private final ElasticsearchVacancySearchService elasticsearchVacancySearchService;
 
-    @GetMapping("/asc")
-    public ResponseEntity<List<VacancyDocumentDto>> findAllAsc() {
-        return ResponseEntity.ok().body(vacancySearchService.findAllAsc());
+    @GetMapping
+    public List<VacancyDocumentDto> findAll(@Valid @RequestBody ElasticsearchVacancyPageDto pageDto) {
+        return elasticsearchVacancySearchService.findAll(pageDto);
     }
 
-    @GetMapping("/desc")
-    public ResponseEntity<List<VacancyDocumentDto>> findAllDesc() {
-        return ResponseEntity.ok().body(vacancySearchService.findAllDesc());
-    }
 }
